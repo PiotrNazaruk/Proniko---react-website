71cfe603ca310dfa8c7700d1073c96dffc95175a
PiotrNazaruk <nazaruk89@gmail.com>
2019-12-18 23:10:28 +0100

    add react helmet

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,9 +3,15 @@ import React from 'react';
 import Navigation from '../components/Navigations';
 import Footer from '../components/Footer';
 import Burger from '../components/Burger';
+import Helmet from 'react-helmet'
 const Layout = (props) => {
   return ( 
     <>
+    <Helmet>
+          <meta charSet="utf-8" />
+          <title>PRONIKO</title>
+          <link href="https://fonts.googleapis.com/css?family=Source+Code+Pro:400,700&display=swap&subset=latin-ext" rel="stylesheet"></link>
+        </Helmet>
     <Navigation />
     <Burger click={props.click}/>
     {props.children}
