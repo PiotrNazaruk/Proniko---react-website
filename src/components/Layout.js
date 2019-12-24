9b3e009aebf77fae459abefbd13076e52eaaf8f7
PiotrNazaruk <nazaruk89@gmail.com>
2019-12-24 14:37:29 +0100

    adding seo

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -9,7 +9,10 @@ const Layout = (props) => {
     <>
     <Helmet>
           <meta charSet="utf-8" />
-          <title>PRONIKO</title>
+          <title>PRONIKO - transport osób</title>
+          <meta name='keywords' content= "wynajem busa, Zielona Góra, przewóz osób, eventy, wesela, imprezy, wynajmę busa Zielona Góra, bus na na lotnisko Zielona Góra, bus do Wólki Kosowskiej Zielona Góra"/>
+        
+          <meta name='description' content= "Wynajem busa Zielona Góra, eventy, przejazd na lotniska, wesela i wiele innych"/>
           <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:400,700&display=swap&subset=latin-ext" rel="stylesheet"></link>
         </Helmet>
     <Navigation />
